3ca7dd101705d11d9cf94519c681f16997efafe0
Ananya Harshe <anaharshe@yahoo.com>
2026-03-06 00:39:24 -0500

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import Footer from "@/components/footer";
 export default function Home() {
   return (
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <main style={{ flex: 1 }}>
-        {/* Your main content goes here */}
+      <main style={{ flex: 1}}>      
       </main>
       <Footer />
     </div>
-  );
+  )
 }
